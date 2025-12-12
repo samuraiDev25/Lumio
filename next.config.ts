672d567e69f32eb4a2d1a2374ab65563f777ecfe
Artur_Islakaev <artur_islakaev@mail.ru>
2025-12-12 13:01:05 +0500

    chore: fix scss imports

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from 'next';
+import path from 'path';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  sassOptions: {
+    includePaths: [path.join(process.cwd(), 'src')],
+  },
 };
-
 export default nextConfig;
