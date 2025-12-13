76a461385a78621ece66474549108340bf823eb4
kliv <kersainvas@gmail.com>
2025-12-13 17:07:27 +0300

    uploaded the latest version

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,36 @@
 'use client';
 
-import { Header } from '@/widgets/header/ui';
+import { Checkbox, Pagination, TextArea, TextField } from '@/shared/ui';
+import Tab from '@/shared/ui/tab/Tab';
 
 export default function Home() {
   return (
     <div>
-      <Header />
       <h1>MAIN PAGE</h1>
+      <Tab
+        items={[
+          {
+            label: 'checkbox',
+            value: 'checkbox',
+            children: <Checkbox checked={true} onChange={() => true} />,
+          },
+          {
+            label: 'textarea',
+            value: 'textarea',
+            children: <TextArea />,
+          },
+          {
+            label: 'pagination',
+            value: 'pagination',
+            children: <Pagination totalPages={1} />,
+          },
+          {
+            label: 'textfield',
+            value: 'textfield',
+            children: <TextField />,
+          },
+        ]}
+      />
     </div>
   );
 }
