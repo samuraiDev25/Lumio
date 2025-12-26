82f33cb4694af30b3a42efee2639ddfc87d2b377
ElenaKuklina <cuklina.lena@gmail.com>
2025-12-27 00:09:55 +0300

    Update page.tsx
    fix: remove duplicate title from login page

--- app/auth/sign-in/page.tsx
+++ app/auth/sign-in/page.tsx
@@ -5,7 +5,6 @@ import { LoginForm } from '@/features/auth/ui/loginForm/LoginForm';
 export default function SignIn() {
   return (
     <div>
-      <h1>SIGN IN</h1>
       <LoginForm />
     </div>
   );
