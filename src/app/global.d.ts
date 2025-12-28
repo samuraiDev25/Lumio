fd67f9626a7bd5d2550b4e82473e2e166a274cb3
Artur Islakaev <artur_islakaev@mail.ru>
2025-12-28 10:12:16 +0500

    Revert "Uc 3 password recovery"

--- src/app/global.d.ts
+++
@@ -1,5 +0,0 @@
-declare global {
-  declare type AppStore = import('./store').AppStore;
-}
-
-export {};
--- src/widgets/sidebar/index.ts
+++
@@ -1,8 +0,0 @@
-export { Sidebar } from '@/widgets/sidebar/ui/Sidebar/Sidebar';
-export { SidebarItem } from './ui/Sidebar/SidebarItem/SidebarItem';
-export type {
-  SidebarProps,
-  SidebarItemType,
-  SidebarItemProps,
-  IconComponent,
-} from './ui/Sidebar/types';
