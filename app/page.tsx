6225bad2442474b555efaf2c9091c51bcf8e00b6
JuliStrom <jstrom312@gmail.com>
2026-01-18 19:19:54 +0300

    feat:add component Sidebar to page.tsx for authorization users

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@
 import { useMeQuery } from '@/features/auth/api/authApi';
 import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
+import { Sidebar } from '@/widgets/sidebar/ui';
 
 export default function Home() {
   const { data: user, isLoading, isError } = useMeQuery();
@@ -25,7 +26,8 @@ export default function Home() {
   return (
     <div>
       <h1>MAIN PAGE</h1>
-      <div>
+      <Sidebar />
+      <div style={{ marginLeft: '15em' }}>
         <div>Имя {user.username}</div>
         <div>id: {user.userId}</div>
         <div>email: {user.email}</div>
