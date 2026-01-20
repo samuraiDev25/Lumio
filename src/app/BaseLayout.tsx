3390779e653ed692717019163742cb84697bb2b5
roma1524 <kulikr.js@mail.ru>
2026-01-20 20:06:56 +0300

    FIX refactoring Header / if registered no btns

--- src/app/BaseLayout.tsx
+++ src/app/BaseLayout.tsx
@@ -2,11 +2,18 @@
 
 import { PropsWithChildren } from 'react';
 import s from '@/app/BaseLayout.module.scss';
-import { Header } from '@/widgets/header/ui';
 import { Providers } from '@/app/Providers';
 import { ToastContainer } from 'react-toastify';
+import { useMeQuery } from '@/features/auth/api/authApi';
+import { Header } from '@/widgets/header';
 
 export const BaseLayout = ({ children }: PropsWithChildren) => {
+  const { isLoading } = useMeQuery();
+
+  if (isLoading) {
+    return null;
+  }
+
   return (
     <div className={s.layoutContainer}>
       <Providers>
