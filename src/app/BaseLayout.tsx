e040a945ccf8706a7ebc2bf377f333efc6462f5a
roma1524 <kulikr.js@mail.ru>
2026-01-21 20:30:50 +0300

    return toast

--- src/app/BaseLayout.tsx
+++ src/app/BaseLayout.tsx
@@ -4,6 +4,7 @@ import { PropsWithChildren } from 'react';
 import s from '@/app/BaseLayout.module.scss';
 import { useMeQuery } from '@/features/auth/api/authApi';
 import { Header } from '@/widgets/header';
+import { ToastContainer } from 'react-toastify';
 
 export const BaseLayout = ({ children }: PropsWithChildren) => {
   const { isLoading } = useMeQuery();
@@ -16,6 +17,7 @@ export const BaseLayout = ({ children }: PropsWithChildren) => {
     <div className={s.layoutContainer}>
       <Header />
       {children}
+      <ToastContainer position="bottom-right" autoClose={4000} />
     </div>
   );
 };
