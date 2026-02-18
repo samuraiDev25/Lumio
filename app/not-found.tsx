9cfee79e2c43686cdda0d9a1af113c493030c956
roma1524 <kulikr.js@mail.ru>
2026-02-18 23:38:17 +0300

    add not found page and base css for it

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,9 +1,22 @@
-'use client';
+import Link from 'next/link';
+import s from './not-found.module.scss';
+import MainLayout from '@/app/MainLayout';
+import { APP_ROUTES } from '@/shared/lib/routes';
 
-export default function notFound() {
+export default function NotFound() {
   return (
-    <div>
-      <h1>NOT FOUND</h1>
-    </div>
+    <MainLayout>
+      <div className={s.container}>
+        <h1 className={s.code}>404</h1>
+
+        <h2 className={s.title}>Page not found</h2>
+
+        <p className={s.description}>
+          The page you are looking for does not exist or was moved.
+        </p>
+
+        <Link href={APP_ROUTES.ROOT}>Back to Home</Link>
+      </div>
+    </MainLayout>
   );
 }
