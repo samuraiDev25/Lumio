98dab8df7888e09d12deec3174c053f6e352eb01
ElenaKuklina <cuklina.lena@gmail.com>
2026-02-19 18:16:28 +0300

    refactor/sidebar-optimize: navigation, prevent hard-reloads and unnecessary re-renders

--- src/widgets/sidebar/ui/Sidebar.tsx
+++ src/widgets/sidebar/ui/Sidebar.tsx
@@ -1,6 +1,5 @@
 'use client';
-import { usePathname, useRouter } from 'next/navigation';
-import * as NavigationMenu from '@radix-ui/react-navigation-menu';
+import { usePathname } from 'next/navigation';
 import s from './Sidebar.module.scss';
 import { SidebarItemType, SidebarProps } from './types';
 import {
@@ -22,6 +21,7 @@ import { SidebarItem } from './SidebarItem';
 import { LogOutButton } from '@/features/auth/ui/logout';
 import { SIDEBAR_ROUTES } from '@/shared/lib/routes';
 import { useMeQuery } from '@/features/auth/api/authApi';
+import { clsx } from 'clsx';
 
 const mainItems: SidebarItemType[] = [
   {
@@ -29,47 +29,47 @@ const mainItems: SidebarItemType[] = [
     label: 'Feed',
     iconOutline: HomeOutline,
     iconFilled: Home,
-    href: `${SIDEBAR_ROUTES.FEED}`,
+    href: SIDEBAR_ROUTES.FEED,
   },
   {
     id: 'create',
     label: 'Create',
     iconOutline: PlusCircleOutline,
     iconFilled: PlusCircle,
-    href: `${SIDEBAR_ROUTES.CREATE}`,
+    href: SIDEBAR_ROUTES.CREATE,
   },
   {
     id: 'myProfile',
     label: 'My Profile',
     iconOutline: PersonOutline,
     iconFilled: Person,
-    href: `${SIDEBAR_ROUTES.PROFILE}`,
+    href: SIDEBAR_ROUTES.PROFILE,
   },
   {
     id: 'messenger',
     label: 'Messenger',
     iconOutline: MessageCircleOutline,
     iconFilled: MessageCircle,
-    href: `${SIDEBAR_ROUTES.MESSENGER}`,
+    href: SIDEBAR_ROUTES.MESSENGER,
   },
   {
     id: 'search',
     label: 'Search',
     iconOutline: SearchOutline,
-    href: `${SIDEBAR_ROUTES.SEARCH}`,
+    href: SIDEBAR_ROUTES.SEARCH,
   },
   {
     id: 'statistics',
     label: 'Statistics',
     iconOutline: TrendingUpOutline,
-    href: `${SIDEBAR_ROUTES.STATISTICS}`,
+    href: SIDEBAR_ROUTES.STATISTICS,
   },
   {
     id: 'favorites',
     label: 'Favorites',
     iconOutline: HeartOutline,
     iconFilled: Heart,
-    href: `${SIDEBAR_ROUTES.FAVORITES}`,
+    href: SIDEBAR_ROUTES.FAVORITES,
   },
 ];
 
@@ -82,8 +82,8 @@ export const Sidebar = ({
   style,
 }: SidebarProps) => {
   const pathname = usePathname();
-  const router = useRouter();
   const { data: currentUser } = useMeQuery();
+
   const items = mainItems.map((item) => {
     if (item.id === 'myProfile' && currentUser?.userId) {
       return {
@@ -93,64 +93,50 @@ export const Sidebar = ({
     }
     return item;
   });
+
   const activeItem =
     externalActiveItem ||
     (pathname.startsWith('/profile') ? 'myProfile' : '') ||
-    mainItems.find((item) => pathname === item.href)?.id ||
+    items.find((item) => pathname === item.href)?.id ||
     '';
+
   const isDisabled = variant === 'disabled';
-  const handleItemClick = (itemId: string) => {
-    if (isDisabled) return;
 
-    const item = items.find((i) => i.id === itemId);
+  const logoutClasses = clsx(
+    s['nav-link'],
+    activeItem === 'logout' && s.active,
+    isDisabled && s.disabled,
+    s.logout,
+  );
 
-    if (item?.href) {
-      console.log(`Навигация на страницу: ${item.href}`);
-      router.push(item.href);
-    } else if (itemId === 'create') {
-      console.log('Открыть модалку создания поста');
-    }
-
-    onItemClickAction?.(itemId);
-  };
   return (
-    <aside className={`${s.sidebar} ${s[variant]} ${className}`} style={style}>
-      <NavigationMenu.Root orientation="vertical">
-        <nav className={s['nav-container']}>
-          <NavigationMenu.List className={s['nav-list']}>
-            {items.map((item) => (
-              <SidebarItem
-                key={item.id}
-                item={item}
-                isActive={activeItem === item.id}
-                isDisabled={isDisabled}
-                onClickAction={() => handleItemClick(item.id)}
-                className={s[item.id === 'myProfile' ? 'my-profile' : item.id]}
-              />
-            ))}
-          </NavigationMenu.List>
+    <aside className={clsx(s.sidebar, s[variant], className)} style={style}>
+      <nav className={s['nav-container']}>
+        <ul className={s['nav-list']}>
+          {items.map((item) => (
+            <SidebarItem
+              key={item.id}
+              item={item}
+              isActive={activeItem === item.id}
+              isDisabled={isDisabled}
+              onClickAction={() => onItemClickAction?.(item.id)}
+            />
+          ))}
+        </ul>
 
-          {/* Log Out */}
-          <div className={s['logout-section']}>
-            <NavigationMenu.Item>
-              <LogOutButton
-                variant="link"
-                onLogout={() => {
-                  if (!isDisabled) {
-                    onLogoutAction?.();
-                  }
-                }}
-                className={`${s['nav-link']} ${activeItem === 'logout' ? s.active : ''} ${isDisabled ? s.disabled : ''} ${s.logout}`}
-              >
-                <div className={s['icon-container']}>
-                  <LogOutOutline className={s.icon} />
-                </div>
-                <span className={s['text-container']}>Log Out</span>
-              </LogOutButton>
-            </NavigationMenu.Item>
-          </div>
-        </nav>
-      </NavigationMenu.Root>
+        <div className={s['logout-section']}>
+          <LogOutButton
+            variant="link"
+            onLogout={() => !isDisabled && onLogoutAction?.()}
+            className={logoutClasses}
+          >
+            <div className={s['icon-container']}>
+              <LogOutOutline className={s.icon} />
+            </div>
+            <span className={s['text-container']}>Log Out</span>
+          </LogOutButton>
+        </div>
+      </nav>
     </aside>
   );
 };
