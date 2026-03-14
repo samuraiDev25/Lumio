6b12c887c81bfc54c0981cc0eabbf12b72c0bd27
vitaliy <buryanov9@yandex.ru>
2026-03-14 19:56:31 +0300

    feat: add my payments table with pagination

--- src/features/payments/api/paymentsApi.ts
+++ src/features/payments/api/paymentsApi.ts
@@ -8,7 +8,6 @@ import {
   GetMyPaymentsRequest,
   GetMyPaymentsResponse,
 } from '@/features/payments/api/paymentsApi.types';
-import { MOCK_PAYMENTS } from '@/features/payments/api/paymentsService';
 
 export const paymentsApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
@@ -23,42 +22,12 @@ export const paymentsApi = baseApi.injectEndpoints({
       }),
     }),
 
-    getMyPayments: builder.query<
-      GetMyPaymentsResponse,
-      GetMyPaymentsRequest | undefined
-    >({
+    getMyPayments: builder.query<GetMyPaymentsResponse, GetMyPaymentsRequest>({
       query: (params) => ({
         url: '/api/v1/payments/my-payments',
         method: 'GET',
         params,
       }),
-      // TODO: remove mock when backend has real data
-      transformResponse: (
-        response: GetMyPaymentsResponse,
-        _meta,
-        arg?: GetMyPaymentsRequest,
-      ): GetMyPaymentsResponse => {
-        if (response.totalCount !== 0) {
-          return response;
-        }
-
-        const pageSize = response.pageSize || arg?.pageSize || 10;
-        const page = response.page || arg?.pageNumber || 1;
-
-        const pagesCount = Math.ceil(MOCK_PAYMENTS.length / pageSize);
-
-        const startIndex = (page - 1) * pageSize;
-        const endIndex = page * pageSize;
-
-        return {
-          ...response,
-          pagesCount,
-          page,
-          pageSize,
-          totalCount: MOCK_PAYMENTS.length,
-          items: MOCK_PAYMENTS.slice(startIndex, endIndex),
-        };
-      },
     }),
 
     updateAutoRenewal: builder.mutation<void, UpdateAutoRenewalRequest>({
