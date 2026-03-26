30818cf74a5db36252a5d2eb3aa1ac995e9b9091
kersain <kersainvas@gmail.com>
2026-03-26 20:35:06 +0300

    fix merge conflicts

--- src/widgets/ProfileAccount/ui/UpdateAutoRenewal/UpdateAutoRenewal.tsx
+++ src/widgets/ProfileAccount/ui/UpdateAutoRenewal/UpdateAutoRenewal.tsx
@@ -1,4 +1,4 @@
-'use client';
+'use client'
 
 import { useEffect, useState } from 'react';
 import { Checkbox } from '@/shared/ui';
@@ -12,20 +12,18 @@ import { useUpdateAutoRenewalMutation } from '@/features/payments/api/paymentsAp
 
 type Props = {
   autoRenewal: boolean;
-  accountType?: AccountType;
   onAutoRenewalChangeAction?: (
     autoRenewal: boolean,
-    newAccountType?: AccountType,
   ) => void;
 };
 
 export const UpdateAutoRenewal = ({
   autoRenewal,
-  accountType,
   onAutoRenewalChangeAction,
 }: Props) => {
   const [isAutoRenewal, setIsAutoRenewal] = useState(autoRenewal);
   const [updateAutoRenewal, { isLoading }] = useUpdateAutoRenewalMutation();
+
   const dispatch = useAppDispatch();
 
   const { data: me, isLoading: isMeLoading } = useMeQuery();
@@ -55,20 +53,15 @@ export const UpdateAutoRenewal = ({
       await updateAutoRenewal({
         profileId: String(profile?.id),
         autoRenewal: newValue,
-      }).unwrap();
+    }).unwrap();
 
-      toast.success('Auto-renewal updated');
+    toast.success('Auto-renewal updated');
 
-      let newAccountType: AccountType | undefined = accountType;
-      if (accountType === 'business') {
-        newAccountType = 'personal';
-      }
-
-      onAutoRenewalChangeAction?.(newValue, newAccountType);
+    onAutoRenewalChangeAction?.(newValue);
     } catch (error) {
       setIsAutoRenewal(previousValue);
 
-      handleNetworkError({
+    handleNetworkError({
         error,
         dispatch,
         handle400Error: () => {
@@ -86,7 +79,7 @@ export const UpdateAutoRenewal = ({
         handleUnknownError: () => {
           toast.error('An unexpected error occurred.');
         },
-      });
+    });
     }
   };
 
@@ -98,4 +91,4 @@ export const UpdateAutoRenewal = ({
       disabled={isLoading}
     />
   );
-};
+}
